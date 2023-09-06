8e644b9a9aec0928ba212b557ab6b9f7a7c14477
alaminsahed <alaminsahed97@gmail.com>
2023-09-06 14:18:21 +0600

    feat: basic fetch

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import './App.css';
 import SuperHeroesPage from "./components/SuperHeroesPage";
 import HomePage from "./components/HomePage";
 import RQSuperHeroesPage from "./components/RQSuperHeroesPage";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const queryClient = new QueryClient()
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='super-heroes' element={<SuperHeroesPage />} />
-          <Route path='rq-super-heroes' element={<RQSuperHeroesPage />} />
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='super-heroes' element={<SuperHeroesPage />} />
+            <Route path='rq-super-heroes' element={<RQSuperHeroesPage />} />
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -1,9 +1,27 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+
+const fetchData = () => {
+    return axios.get('http://localhost:4001/superheroes')
+}
 
 const RQSuperHeroesPage = () => {
+    const { isLoading, data } = useQuery({ queryKey: ['superheroes'], queryFn: fetchData });
+
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+
     return (
         <div>
             <h1>RQSuperHeroesPage</h1>
+            {
+                data && data.data.map(superhero => (
+                    <div key={superhero.name}>{superhero.name}</div>
+                ))
+            }
         </div>
     );
 };
