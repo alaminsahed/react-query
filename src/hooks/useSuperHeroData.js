1a51c05b693d9864af6953de09fe2347f78e1260
alaminsahed <alaminsahed97@gmail.com>
2023-09-13 18:14:05 +0600

    feat: optimization mutation

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -21,15 +21,38 @@ const addSuperHero = hero => {
 export const useAddSuperHeroData = () => {
     const queryClient = useQueryClient();
     return useMutation(addSuperHero, {
-        onSuccess: (data) => {
-            //queryClient.invalidateQueries('superheroes')
-            // set queries update the cache value and show the updated data
-            queryClient.setQueryData(['superheroes'], (oldQueryData) => {
-                console.log("++", oldQueryData, data);
+        // onSuccess: (data) => {
+        //     //queryClient.invalidateQueries('superheroes')
+        //     // set queries update the cache value and show the updated data
+        //     queryClient.setQueryData(['superheroes'], (oldQueryData) => {
+        //         console.log("++", oldQueryData, data);
+        //         return {
+        //             ...oldQueryData.data, data: [...oldQueryData.data, data.data]
+        //         }
+        //     })
+        // }
+        onMutate: async newHero => {
+            await queryClient.cancelQueries('superheroes')
+            const previousHeroData = queryClient.getQueryData(['superheroes'])
+            console.log(newHero);
+            queryClient.setQueryData(['superheroes'], oldQueryData => {
                 return {
-                    ...oldQueryData.data, data: [...oldQueryData.data, data.data]
+                    ...oldQueryData,
+                    data: [
+                        ...oldQueryData.data,
+                        { id: oldQueryData?.data?.length + 1, ...newHero }
+                    ]
                 }
             })
+            return { previousHeroData }
+        },
+        onError: (_err, _newTodo, context) => {
+            // if get error then rollback the data
+            queryClient.setQueryData(['superheroes'], context.previousHeroData)
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries('superheroes')
         }
+        /**Optimistic Update End */
     })
 }
